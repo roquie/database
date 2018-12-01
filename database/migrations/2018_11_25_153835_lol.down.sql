c2fd7721a0f6016891c135f776592fb669ce09b1
roquie <roquie0@gmail.com>
2018-12-02 01:01:58 +0300

    Remove dirty files.

--- database/migrations/2018_11_25_153835_lol.down.sql
+++
@@ -1,3 +0,0 @@
--- migration: 2018_11_25_153835_lol.down.sql
-
-drop table if exists lols;
--- database/migrations/2018_11_25_153835_lol2.up.sql
+++
@@ -1,7 +0,0 @@
--- migration: 2018_11_25_153835_lol.up.sql
-
-
-CREATE TABLE lols2 (
-  migrat2ion Character Varying (255) NOT NULL,
-  bat2ch Integer NOT NULL
-  );
